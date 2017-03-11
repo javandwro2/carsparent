0f146440b95b6fb29a24f322cab8915da7c48222
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-11 13:48:43 +0100

    Initial commit

---
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/App.java
@@ -0,0 +1,13 @@
+package pl.jwrabel.javandwro2.cars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/Car.java
@@ -0,0 +1,20 @@
+package pl.jwrabel.javandwro2.cars;
+
+/**
+ * Created by jakubwrabel on 11.03.2017.
+ */
+public class Car {
+	String brand;
+	String model;
+	int manufactureYear;
+	double power;
+
+	public Car(String text){
+
+	}
+
+	@Override
+	public String toString() {
+		return "Car{}";
+	}
+}
