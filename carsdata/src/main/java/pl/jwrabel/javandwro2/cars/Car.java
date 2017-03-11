c29789da1102ac650870962ca997bcaf1a689b1d
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-11 13:53:48 +0100

    Added .gitignore

--- carsdata/src/main/java/pl/jwrabel/javandwro2/cars/Car.java
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/Car.java
@@ -4,10 +4,10 @@ package pl.jwrabel.javandwro2.cars;
  * Created by jakubwrabel on 11.03.2017.
  */
 public class Car {
-	String brand;
-	String model;
-	int manufactureYear;
-	double power;
+	private String brand;
+	private String model;
+	private int manufactureYear;
+	private double power;
 
 	public Car(String text){
 
