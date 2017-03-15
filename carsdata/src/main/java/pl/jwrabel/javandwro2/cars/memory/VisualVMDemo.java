235c2e11c42b1f54025528cd0368e0a38937f1fb
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-15 20:58:54 +0100

    update

---
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/memory/VisualVMDemo.java
@@ -0,0 +1,13 @@
+package pl.jwrabel.javandwro2.cars.memory;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jakubwrabel on 15.03.2017.
+ */
+public class VisualVMDemo {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(10000);
+		Thread.sleep(100000);
+	}
+}
