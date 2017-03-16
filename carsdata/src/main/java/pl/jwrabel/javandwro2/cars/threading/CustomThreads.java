24a7563cf8ebba45a261a590286d7f5b0258d782
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-16 19:53:41 +0100

    update

---
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/threading/CustomThreads.java
@@ -0,0 +1,16 @@
+package pl.jwrabel.javandwro2.cars.threading;
+
+/**
+ * Created by jakubwrabel on 16.03.2017.
+ */
+public class CustomThreads {
+	public static void main(String[] args) {
+//		MyThread myThread = new MyThread(100);
+//		myThread.start();
+//
+		// 1
+		// 2
+		// ...
+		// 100
+	}
+}
