57a016ad87ddf7db0c43d2525f3102e8c515a904
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-16 20:05:52 +0100

    update

--- carsdata/src/main/java/pl/jwrabel/javandwro2/cars/threading/CustomThreads.java
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/threading/CustomThreads.java
@@ -1,13 +1,28 @@
 package pl.jwrabel.javandwro2.cars.threading;
 
+import java.util.Random;
+
 /**
  * Created by jakubwrabel on 16.03.2017.
  */
 public class CustomThreads {
 	public static void main(String[] args) {
-		MyThread myThread = new MyThread(100);
-		myThread.start();
-//
+
+		int[] array = new int[100000000];
+		Random random = new Random();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = random.nextInt();
+		}
+
+		for (int i = 0; i < 100; i++) {
+			System.out.println(array[i]);
+		}
+
+		//
+
+//		MyThread myThread = new MyThread(20, 100);
+//		myThread.start();
+////
 		// 1
 		// 2
 		// ...
