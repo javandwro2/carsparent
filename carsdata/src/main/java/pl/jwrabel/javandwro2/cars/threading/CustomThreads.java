7b13672bbbbd9f6e778a61e91c22a7b139744206
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-03-18 10:21:58 +0100

    update

--- carsdata/src/main/java/pl/jwrabel/javandwro2/cars/threading/CustomThreads.java
+++ carsdata/src/main/java/pl/jwrabel/javandwro2/cars/threading/CustomThreads.java
@@ -13,31 +13,38 @@ public class CustomThreads {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = random.nextInt();
 		}
+//
+//		System.out.println("MIN SEARCHING STARTED");
+//		MinThread minThread1 = new MinThread(0,24_999_999,array);
+//		MinThread minThread2 = new MinThread(25_000_000,49_999_999,array);
+//		MinThread minThread3 = new MinThread(50_000_000,74_999_999,array);
+//		MinThread minThread4 = new MinThread(75_000_000,99_999_999,array);
+//		minThread1.start();
+//		minThread2.start();
+//		minThread3.start();
+//		minThread4.start();
+//
+//		minThread1.join();
+//		minThread2.join();
+//		minThread3.join();
+//		minThread4.join();
+//
+//		int[] mins = {minThread1.getMin(), minThread2.getMin(), minThread3.getMin(), minThread4.getMin()};
+//
+//		int min = mins[0];
+//		for (int i = 1; i <4; i++) {
+//			if(mins[i] < min){
+//				min = mins[i];
+//			}
+//		}
+//
+//		System.out.println("MIN: " + min);
 
-		System.out.println("MIN SEARCHING STARTED");
-		MinThread minThread1 = new MinThread(0,24_999_999,array);
-		MinThread minThread2 = new MinThread(25_000_000,49_999_999,array);
-		MinThread minThread3 = new MinThread(50_000_000,74_999_999,array);
-		MinThread minThread4 = new MinThread(75_000_000,99_999_999,array);
-		minThread1.start();
-		minThread2.start();
-		minThread3.start();
-		minThread4.start();
 
-		minThread1.join();
-		minThread2.join();
-		minThread3.join();
-		minThread4.join();
-
-		int[] mins = {minThread1.getMin(), minThread2.getMin(), minThread3.getMin(), minThread4.getMin()};
-
-		int min = mins[0];
-		for (int i = 1; i <4; i++) {
-			if(mins[i] < min){
-				min = mins[i];
-			}
-		}
-
-		System.out.println("MIN: " + min);
+		MinSearchThread minSearchThread = new MinSearchThread(array, 6);
+		minSearchThread.start();
+		minSearchThread.join();
+		minSearchThread.getMin();
+//		minSearchThread.getResult(); // MinSearchResult (min, średni czas trwania wątku, maksymalny czas trwania itp...)
 	}
 }
